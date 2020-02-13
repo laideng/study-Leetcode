c559f865c69a6833deb93dbeb66ad2ea56740ee2
Menglaiddd <41616107+laideng@users.noreply.github.com>
2020-02-13 18:30:51 +0800

    Update Solution1.java

--- 0001-Two Sum/Java/Solution1.java
+++ 0001-Two Sum/Java/Solution1.java
@@ -1,9 +1,7 @@
 /// Source : https://leetcode-cn.com/problems/two-sum/
 /// Author : mengld
 /// Time   : 2020-2-13
-/*
-Brute force enumeration
-*/
+//  暴力枚举
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
@@ -15,18 +13,5 @@ class Solution {
             }
         }
         return null;
-    }
-
-
-
-    public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
-        Solution s = new Solution();
-        int[] result = s.twoSum(nums, target);
-        for (int i: result) {
-            System.out.println(nums[i]);
-        }
-
     }
 }
